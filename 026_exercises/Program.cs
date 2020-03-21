a121cbf438ff0070fbaaec45b56c9aca0124f562
tamas-jaray <jaray.tamas@gmail.com>
2020-03-21 23:18:55 +0100

    Add project files.

---
+++ 026_exercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _026_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(DateTime.Today);
+            
+            
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            
+        }
+    }
+}
---
+++ 026_exercises/Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _026_exercises
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Stack
+    {
+        public ArrayList listOfObjects;
+        public List<string> objectsAsString;
+        /// <summary>
+        /// Parameterless constructor of the Stack class.
+        /// </summary>
+        public Stack()
+        {
+            listOfObjects = new ArrayList();
+            objectsAsString = new List<string>();
+        }
+        /// <summary>
+        /// Pushes the object on top of the stack.
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+               listOfObjects.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("Not valid object");
+            }
+        }
+        /// <summary>
+        /// Returns the last object on the stack and removes it.
+        /// </summary>
+        /// <returns></returns>
+        public string Pop()
+        {
+            if (listOfObjects.Count != 0)
+            {
+                var lastObject = listOfObjects[listOfObjects.Count - 1];
+
+                listOfObjects.RemoveAt(listOfObjects.Count - 1);
+
+                return lastObject.ToString();
+
+            }
+            else
+            {
+                throw new InvalidOperationException("Empty list");
+            }
+
+        }
+        /// <summary>
+        /// Clears the list of objects.
+        /// </summary>
+        public void Clear()
+        {
+            listOfObjects.Clear();
+        }
+    }
+}
